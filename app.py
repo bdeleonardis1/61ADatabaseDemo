d1007a121440af515ce38cc9ee6a7cd210b717e7
bdeleonardis1 <bdeleonardis1@gmail.com>
2018-11-01 14:48:06 -0700

    Add all database related code

--- app.py
+++ app.py
@@ -33,7 +33,42 @@ def check_answer():
     input_answer = request.args['input_answer']
     real_answer = request.args['real_answer']
     correct_answer_string = 'Correct' if input_answer.lower() == real_answer.lower() else 'Incorrect'
+
+    con = get_db()
+    cur = con.cursor()
+    if correct_answer_string == 'Correct':
+        cur.execute("INSERT INTO answers VALUES (1);")
+    else:
+        cur.execute("INSERT INTO answers VALUES (0);")
+    con.commit() 
+
     return render_template('answer.html', input_answer=input_answer, real_answer=real_answer, correct_answer_string=correct_answer_string)
+
+import sqlite3
+DATABASE = 'jeopardydb.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/stats', methods=['GET'])
+def get_stats():
+    cur = get_db().cursor()
+    cur.execute("SELECT COUNT(*) FROM Answers WHERE Correct=1;")
+    correct = cur.fetchone()[0]
+    cur.execute("SELECT COUNT(*) FROM Answers WHERE Correct=0;")
+    incorrect = cur.fetchone()[0]
+
+    return "Users have answered {} correctly and {} incorrectly for {:.2f}%".format(correct, incorrect, correct / (correct + incorrect))
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
